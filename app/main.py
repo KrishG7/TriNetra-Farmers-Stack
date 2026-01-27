13c6e25c0a3e0823429aa98475faa1237e3b373e
Krish Gupta <Krish@MacBook-Air.local>
2026-01-27 14:25:20 +0530

    Allow all CORS origins for deployment

--- app/main.py
+++ app/main.py
@@ -24,10 +24,7 @@ app = FastAPI(title="TriNetra API", version="2.0.0")
 # --- CORS (Allow Frontend to talk to Backend) ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
